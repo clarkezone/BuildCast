b5a04876424ce87f6ee2204b22ed972f127b804f
James Clarke <james@clarkezone.net>
2017-10-23 16:37:25 +0200

    Demo 5 Smart Ink

--- src/BuildCast/Views/InkNote.xaml.cs
+++ src/BuildCast/Views/InkNote.xaml.cs
@@ -103,6 +103,20 @@ namespace BuildCast.Views
                         AddHeart(drawing);
                         RemoveInkStrokes(drawing);
                     }
+                    else if (drawing.DrawingKind == InkAnalysisDrawingKind.Triangle || drawing.DrawingKind == InkAnalysisDrawingKind.EquilateralTriangle)
+                    {
+                        if (string.IsNullOrEmpty(customEmojiGlyph))
+                        {
+                            await PickEmoji();
+                        }
+
+                        if (!string.IsNullOrEmpty(customEmojiGlyph))
+                        {
+                            AddEmoji(drawing, customEmojiGlyph);
+                        }
+
+                        RemoveInkStrokes(drawing);
+                    }
                 }
 
                 _inkAnalyzer.ClearDataForAllStrokes();
